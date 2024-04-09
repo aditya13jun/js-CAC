eaa8d0e595a695d78225d072e58992ffe0892f69
Aditya Harsh <114324933+aditya13jun@users.noreply.github.com>
2024-04-09 19:02:40 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144552
+let accountEmail = "aditya13jun@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountEmail = "123fv@gmail.com"
+
+accountPassword = "54321"
+
+accountCity = "Mumbai"
+
+console.log(accountId)
+
+/*
+Prefer not to use var...
+*/
+
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState ])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code in newer version...
+
+//alert("3+3"); using node.js not browser
+
+//Code readabality should be high...
+
+let name = 'Aditya'
+let age = 21
+let isLoggedIn = false
+
+//number => 2^53
+//bigInt if number is too big
+// string => " "
+//boolean => true/false
+//null => Standolone value (object)
+//Undefined => Empty
+//Symbol => uniqueness
+
+//Object 
+
+console.log(typeof undefined)

4bf7bb5d8212d54f69e547aed60c9ce8bce75ec4
Aditya Harsh <114324933+aditya13jun@users.noreply.github.com>
2024-04-09 20:00:42 +0000

    comparision added

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null>=0)
+//comparision converts null to number, treating it as 0...
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// === => checks the data type too...
+console.log("2" === 2)

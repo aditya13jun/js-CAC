a80e85e764aa46c83d631f80d2faed3e5bb59ad4
Aditya Harsh <114324933+aditya13jun@users.noreply.github.com>
2024-04-09 19:31:51 +0000

    updated conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber)
-console.log(typeof stringSomeNumber)
+//console.log(stringSomeNumber)
+//console.log(typeof stringSomeNumber)
+
+//-------------------------------
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+
+let str1 = "hello"
+let str2 = "aditya"
+let str3 = str1 + " " + str2
+// console.log(str3)
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1= num2= num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+++gameCounter
+console.log(gameCounter)
